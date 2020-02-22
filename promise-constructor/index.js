766f60f1fbb747d09325e61c214166a2dda0c7c4
Nathan Sanford <nwsanfor24@gmail.com>
2020-02-22 11:41:49 -0500

    changed everthing

--- promise-constructor/index.js
+++ promise-constructor/index.js
@@ -1,20 +1,19 @@
-function waitFor(seconds, cb) {
-    if (isNaN(seconds) || seconds < 1) {
-      return cb(Error("Parameter 'seconds' must be a positive number!"));
-    }
-  
-    setTimeout(function() {
-      cb(null, "Success!");
-    }, seconds * 1000);
-  }
-  
-  
-  waitFor(2, function(err, msg) {
-    if (err) {
-      console.log(err);
-      return;
-    }
-  
-    console.log(msg);
-  })
-  
+function waitFor(seconds) {
+    return new Promise(function (resolve, reject) {
+        if (isNaN(seconds) || seconds < 1) {
+            return reject(new Error("Parameter 'seconds' must be a positive number!"));
+        }
+
+        setTimeout(function () {
+            resolve("Success!");
+        }, seconds * 1000);
+    });
+}
+
+waitFor(2)
+    .then(function (msg) {
+        console.log(msg);
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
