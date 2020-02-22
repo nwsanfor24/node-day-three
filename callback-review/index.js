c5109fed0c9bc8100e196797a1ce8f3b88bf8fe6
Nathan Sanford <nwsanfor24@gmail.com>
2020-02-22 10:24:48 -0500

    added functions

--- callback-review/index.js
+++ callback-review/index.js
@@ -8,7 +8,36 @@ fs.readFile("animals.json", "utf8", function(err, data) {
   // Parse the JSON string to an object
   const animalJSON = JSON.parse(data);
 
+
+
   // Create two new arrays to contain the cats and dogs objects
   const dogs = [];
   const cats = [];
+
+  animalJSON.forEach(function(animal) {
+      if (animal.species === "dog") {
+          dogs.push(animal);
+      } else if (animal.species === "cat") {
+          cats.push(animal);
+      }
+  });
+
+  const dogJSON = JSON.stringify(dogs, null, 2);
+  const catJSON = JSON.stringify(cats, null, 2);
+
+  fs.writeFile("dogs.json", dogJSON, function(err) {
+      if (err) {
+          throw err;
+      }
+
+      console.log("Successfully wrote to dogs.json file");
+  });
+
+  fs.writeFile("cats.json", catJSON, function(err) {
+      if (err) {
+          throw err;
+      }
+
+      console.log("Successfully wrote to cats.json file");
+  });
 });
