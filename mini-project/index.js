91daed1214c469b864604d4ad75a37d5126f1bc4
Nathan Sanford <nwsanfor24@gmail.com>
2020-02-22 13:26:38 -0500

    blank index.js

--- mini-project/index.js
+++
@@ -1,7 +0,0 @@
-const firstName = document.getElementById("first_name");
-const lastName = document.getElementById("last_name");
-const bio = document.getElementById("bio");
-const location = document.getElementById("location")
-const linkedIn = document.getElementById("linkedin");
-const github = document.getElementById("github");
-
