ca6a5d9151be80bed4cbc72c1106496d35055120
Nathan Sanford <nwsanfor24@gmail.com>
2020-02-22 12:43:27 -0500

    it works

--- async-promise/index.js
+++ async-promise/index.js
@@ -5,11 +5,25 @@ const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
 async function combineAnimals() {
-  try {
+    try {
+        const hamster = await readFileAsync("hamster.json", "utf8");
+        const dog = await readFileAsync("dog.json", "utf8");
+        const cat = await readFileAsync("cat.json", "utf8");
+        const goldfish = await readFileAsync("goldfish.json", "utf8");
 
-  } catch(err) {
+        const animalJSON = [hamster, dog, cat, goldfish].map(JSON.parse);
 
-  }
+        await writeFileAsync(
+            "combined.json",
+            JSON.stringify(sorted, null, 2),
+            "utf8"
+        );
+
+        console.log("Successfully wrote to 'combined.json' file");
+
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 combineAnimals();
